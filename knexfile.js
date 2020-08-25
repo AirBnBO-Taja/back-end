4525b6dcc870a0658a2035924f007e0d9b5d832d
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-08-25 09:06:57 -0700

    test2

--- knexfile.js
+++ knexfile.js
@@ -14,6 +14,11 @@ module.exports = {
     seeds: {
       directory: "./data/seeds",
     },
+    pool:{
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done)
+      }
+    }
   },
 
   production: {
