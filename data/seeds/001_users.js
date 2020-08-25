a8e3856f89af174d80bc51c6317317946c272d67
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-08-25 15:46:15 -0700

    usersComplete

--- data/seeds/001_users.js
+++ data/seeds/001_users.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, name: 'John', email: 'john@gmail.com', password: 12345},
-        {id: 2, name: 'Bob', email: 'bob@gmail.com', password: 12345},
-        {id: 3, name: 'Sam', email: 'sam@gmail.com', password: 12345},
+        {id: 1, name: 'John', email: 'john@gmail.com', password: '12345'},
+        {id: 2, name: 'Bob', email: 'bob@gmail.com', password: '12345'},
+        {id: 3, name: 'Sam', email: 'sam@gmail.com', password: '12345'},
       ]);
     });
 };
