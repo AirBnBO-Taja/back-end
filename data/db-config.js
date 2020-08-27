54435bdcd5cceb0ed3e6772d418ba0ef99cadaa3
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-08-27 10:34:34 -0700

    fixedDBCONFIG

--- data/db-config.js
+++ data/db-config.js
@@ -2,9 +2,3 @@ const knex = require('knex')
 const knexfile = require("../knexfile");
 const enviroment = process.env.NODE_ENV || "development";
 module.exports = knex(knexfile[enviroment]);
-
-describe('server.js', () => {
-    this.intersect('should be in the testing environment', () =>{
-        expect(process.env.DB_ENV).toBe('testing');
-    });
-});
